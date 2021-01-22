1e7db2360b7ce66e8c56a11fb110f99ffe959282
mahinour30 <mahinour1233@gmail.com>
2021-01-22 18:20:07 +0200

    Create icons.js

---
+++ constants/icons.js
@@ -0,0 +1,11 @@
+export const Search = require('../assets/icons/Search.png');
+export const Done = require('../assets/icons/Done.png');
+export const X = require('../assets/icons/X.png');
+export const Avatar = require('../assets/icons/Avatar.png');
+
+export default{
+    Search,
+    Done,
+    X,
+    Avatar
+}
