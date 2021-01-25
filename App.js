40b7edce4b3611b17495a22011cc88c3d005664f
mahinour30 <mahinour1233@gmail.com>
2021-01-25 16:56:12 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,21 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Favorites from './Screens/Favorites';
 import Home from './Screens/Home';
-import {Provider} from 'react-redux';
-import {Store} from './Redux/Store';
 
 const Stack = createStackNavigator();
 
 const App =()=>{
   return(
-    <Provider  store={Store}>
     <NavigationContainer>
    <Stack.Navigator>
     <Stack.Screen name="Home" component={Home}  options={{ headerShown: false }}/>
     <Stack.Screen name="Favorites" component={Favorites} options={{ headerShown: false }} />
    </Stack.Navigator>
     </NavigationContainer> 
-    </Provider>
     
   );
 }
