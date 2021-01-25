101aa03d61cbd344b86c96d0a7a3607bcbb5640e
mahinour30 <mahinour1233@gmail.com>
2021-01-25 23:45:05 +0200

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,23 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import contactReducer from './reducers/reducers';
+import {createLogger} from 'redux-logger';
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const rootReducer = combineReducers({
-  contactReducer: contactReducer
-})
 
-const configureStore = () => createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist:['contactReducer'],
+}
 
-export default configureStore;
+export const rootReducer = combineReducers({
+  contactReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore( persistedReducer, applyMiddleware( createLogger() ));
+
+
+export default store
