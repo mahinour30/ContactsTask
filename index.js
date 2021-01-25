b23c8d4b685a7366039ed57bb362c0544f254697
mahinour30 <mahinour1233@gmail.com>
2021-01-26 00:12:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
-import {persistStore} from 'redux-persist';
-import {PersistGate} from 'redux-persist/es/integration/react';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import store from './redux/store'
 
 const persistedStore = persistStore(store)
 
 const contactsApp = () =>
+  // store for passing data to the whole app
   <Provider store={store}>
+    {/* //// PersistGate for loading the offline data ////  */}
     <PersistGate persistor={persistedStore} loading={null}>
-    <App />
+      <App />
     </PersistGate>
   </Provider>
 
