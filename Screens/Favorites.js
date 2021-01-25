abd01adc1f6fbc7695725d3a63c1517abeae246d
mahinour30 <mahinour1233@gmail.com>
2021-01-26 00:12:22 +0200

    Update Favorites.js

--- Screens/Favorites.js
+++ Screens/Favorites.js
@@ -1,14 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, Platform, StyleSheet, PermissionsAndroid, Image, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
-import {COLORS, FONTS, SIZES, icons} from '../constants';
+import React from 'react';
+import { View, Text, FlatList, StyleSheet, Image, SafeAreaView, TouchableOpacity } from 'react-native';
+import { COLORS, FONTS, icons } from '../constants';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../redux/actions/actions';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 
-const Favorites =({navigation}, props)=>{
+const Favorites = ({ navigation }, props) => {
 
-
+  // to work with redux
   const dispatch = useDispatch();
 
   const deleteCurrent = (key) => dispatch(deleteContact(key))
@@ -16,101 +16,101 @@ const Favorites =({navigation}, props)=>{
   const contacts = useSelector(state => state.contactReducer.contactList)
 
 
-  return(
+  return (
     <SafeAreaView style={styles.Container}>
-        <TouchableOpacity style={{height:20}}
-            
-            onPress={()=> navigation.navigate('Home')}>
-                
-                    <Text style={{...FONTS.h2, color:COLORS.secondary}}>Back</Text> 
-               
-            </TouchableOpacity>
+
+      {/* ////////////// header section //////////// */}
+      <TouchableOpacity style={{ height: 20 }}
+        onPress={() => navigation.navigate('Home')}>
+        <Text style={{ ...FONTS.h2, color: COLORS.secondary }}>Back</Text>
+      </TouchableOpacity>
 
 
-            <FlatList
-             data={contacts}
-             keyExtractor={(item, index) => item.key}
-              renderItem={
-               (data) =>
-               <View  style={styles.listContainer}>
-               
-               <Text style={styles.Title}>
-                 {data.item.name}
-               </Text>
-             
-               <TouchableOpacity  
-               onPress={()=>{deleteCurrent(data.item.key)}}
-               style={styles.Round}>
-                 <View>
-                                     <Image
-                                     style={{ width: 35, height: 35, tintColor: 'red' }}
-                                     source={icons.Trash}
-                                 />
-                            
-               </View>
-     
-                </TouchableOpacity>
+      {/* ////////// favorites list ///////// */}
+      <FlatList
+        data={contacts}
+        keyExtractor={(item, index) => item.key}
+        renderItem={
+          (data) =>
+            <View style={styles.listContainer}>
+
+              <Text style={styles.Title}>
+                {data.item.name}
+              </Text>
+
+              <TouchableOpacity
+                onPress={() => { deleteCurrent(data.item.key) }}
+                style={styles.Round}>
+                <View>
+                  <Image
+                    style={{ width: 35, height: 35, tintColor: 'red' }}
+                    source={icons.Trash}
+                  />
+
                 </View>
 
+              </TouchableOpacity>
+            </View>
 
-
-
-      }
-    />
+        }
+      />
     </SafeAreaView>
 
   )
 }
 
-const mapStateToProps = (state) =>{
-  return{
-    contacts : state.contactReducer.contactList
+
+// to manipulate states
+
+const mapStateToProps = (state) => {
+  return {
+    contacts: state.contactReducer.contactList
   }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-  return{
-    delete :(key) => dispatch(deleteContact(key))
+const mapDispatchToProps = (dispatch) => {
+  return {
+    delete: (key) => dispatch(deleteContact(key))
   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
 
-const styles=StyleSheet.create({
-  Container:{
-    flex:1,
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
     backgroundColor: COLORS.black2
   },
-  Title:{
-    ...FONTS.h1, 
-    color:COLORS.white, 
-    marginTop:10,
-    marginLeft:20
+  Title: {
+    ...FONTS.h1,
+    color: COLORS.white,
+    marginTop: 10,
+    marginLeft: 20
   },
-  Icon:{
-    width:25, 
-    height:25, 
-    borderRadius:50, 
-    marginTop:17,
-    position:'absolute',
-    zIndex:999,
-    left:12,
-    tintColor:COLORS.lightGrey,
+  Icon: {
+    width: 25,
+    height: 25,
+    borderRadius: 50,
+    marginTop: 17,
+    position: 'absolute',
+    zIndex: 999,
+    left: 12,
+    tintColor: COLORS.lightGrey,
   },
-  Round:{
-    width:25,
-    height:25,
-    borderRadius:20,
-    justifyContent:'center', 
-    alignItems:'center',
-    right:30, 
-    position:'absolute', 
-    top:17, 
+  Round: {
+    width: 25,
+    height: 25,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    right: 30,
+    position: 'absolute',
+    top: 17,
   },
-  listContainer:{
-    borderWidth:1,
-    borderColor:COLORS.lightGrey, 
-    flexDirection:'row',
-    padding:10
+  listContainer: {
+    borderWidth: 1,
+    borderColor: COLORS.lightGrey,
+    flexDirection: 'row',
+    padding: 10
   }
 })
