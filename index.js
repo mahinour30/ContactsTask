1649e13d22132a12bf4035502df6196a631a6bb1
mahinour30 <mahinour1233@gmail.com>
2021-01-25 23:44:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import store from './redux/store'
 
-import configureStore from './redux/store';
-
-const store = configureStore();
+const persistedStore = persistStore(store)
 
 const contactsApp = () =>
   <Provider store={store}>
+    <PersistGate persistor={persistedStore} loading={null}>
     <App />
+    </PersistGate>
   </Provider>
 
 AppRegistry.registerComponent(appName, () => contactsApp);
