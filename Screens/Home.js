6b222eaf50bcad060fc2bca57e52455e9935fa94
mahinour30 <mahinour1233@gmail.com>
2021-01-25 16:56:35 +0200

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,227 +1,296 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, Platform, StyleSheet, PermissionsAndroid, Image, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Platform, StyleSheet, PermissionsAndroid, Image, SafeAreaView, TextInput, TouchableOpacity, Pressable,  } from 'react-native';
 import Contacts from 'react-native-contacts';
-import {COLORS, FONTS, SIZES, icons} from '../constants';
+import { COLORS, FONTS, SIZES, icons } from '../constants';
 
 
-const Home =()=>{
+const Home = ({navigation}) => {
 
-  const [contacts, setContacts]= useState(null);
+    const [contacts, setContacts] = useState(null);
+    const [selected, setSelected] = useState([]);
+    const [target, setTarget] = useState('');
+    const [flag , setFlag] = useState(false);
 
+    useEffect(() => {
+        if (Platform.OS === 'ios') {
+            Contacts.getAll((error, contacts) => {
+                if (error) {
+                    throw error;
+                }
+                setContacts(contacts)
+            })
+        } else if (Platform.OS === 'android') {
+            PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+                {
+                    'title': 'Contacts',
+                    'message': 'This app would like to view your contacts.'
+                }
+            ).then(() => {
+                Contacts.getAll()
+                    .then((contacts) => {
 
-  useEffect(() => {
-    if(Platform.OS === 'ios'){
-      Contacts.getAll((error, contacts) => {
-        if(error){
-          throw error;
+                        setContacts(contacts);
+                    })
+                    .catch((e) => { console.log(e); })
+            })
         }
-        setContacts(contacts)
-      })
-    } else if(Platform.OS === 'android'){
-    PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
-      {
-        'title': 'Contacts',
-        'message': 'This app would like to view your contacts.'
-      }
-    ).then(() => {
-      Contacts.getAll()
-    .then((contacts) => {
-      setContacts(contacts);
-    })
-    .catch((e) => { console.log(e); })
-    })
-  }
-  }, []);
+    }, []);
 
-  const getSearch = e =>{
-      const filteredData = contacts.filter((contact)=>{
-        let contactLowerCase = (contact.givenName + '' + contact.familyName).toLowerCase();
-        let searchTermLowerCase = e.toLowerCase();
-        return contactLowerCase.indexOf(searchTermLowerCase)>-1
-      });
-      setContacts(filteredData)
-  }
+    const getSearch = e => {
+        const filteredData = contacts.filter((contact) => {
+            let contactLowerCase = (contact.givenName + '' + contact.familyName).toLowerCase();
+            let searchTermLowerCase = e.toLowerCase();
+            return contactLowerCase.indexOf(searchTermLowerCase) > -1
+        });
+        setContacts(filteredData)
+        setTarget(e)
+    }
 
-  return(
-    <SafeAreaView style={styles.Container}>
-      <View style={{backgroundColor:COLORS.primary}}>
-      <Image
-            style={styles.Icon}
-            source={icons.Search}
-            />
-        <TextInput
-        placeholder='Search'
-        style={styles.Search}
-        onChangeText={(e) => getSearch(e)}
+    return (
+        <SafeAreaView style={styles.Container}>
+            <TouchableOpacity style={{height:20, left:-10, flexDirection:'row-reverse'}}
+            
+            onPress={()=> navigation.navigate('Favorites')}>
+                
+                    {(flag) ? <Text style={{...FONTS.h2, color:COLORS.secondary}}>Next</Text> : <Text style={{...FONTS.h2, color:COLORS.secondary}}>Favorites</Text> }
+               
+            </TouchableOpacity>
+
+            <View style={{ backgroundColor: COLORS.primary }}>
+                <Image
+                    style={styles.Icon}
+                    source={icons.Search}
                 />
-      </View>
+                <TextInput
+                    placeholder='Search'
+                    style={styles.Search}
+                    onChangeText={(e) => getSearch(e)}
+                />
+            </View>
 
-      <FlatList
-      horizontal={true}
-      data = {contacts}
-      renderItem = {({item}) => (
-        <View style={styles.selectContainer}>
-          {(item.hasThumbnail) ?<Image
-          style={styles.Img}
-          source={{uri: item.thumbnailPath}}
-          /> :
-          <View style={styles.Circle}>
-            <Image
-            style={styles.Avatar}
-            source={icons.Avatar}
-            />
-          </View> }
-          <TouchableOpacity  
-          onPress={()=>{console.log('HII')}}
-          style={styles.X}>
-            <View>
-            <Image
-            style={{width:15, height:15}}
-            source={icons.X}
-            />
-          </View>
-           </TouchableOpacity>
-        </View>
-      )}
-      numColumns = {1}
-      keyExtractor = {(item, index) => index}
-      />
+            {selected.length != 0 &&
+                <FlatList
+                    horizontal={true}
+                    data={selected}
+                    renderItem={(item) => (
+                        
+                        <View style={styles.selectContainer}>
+                            {(item.item.hasThumbnail) ? <Image
+                                style={styles.Img}
+                                source={{ uri: item.item.thumbnailPath }}
+                            /> :
+                                <View style={styles.Circle}>
+                                    <Image
+                                        style={styles.Avatar}
+                                        source={icons.Avatar}
+                                    />
+                                </View>}
+                            <TouchableOpacity
+                                onPress={() => {
+                                    console.log("unselected==>", item.item)
+                                    let temp = [...selected]
+                                    temp.pop(item.item)
+                                setSelected(temp)
+                                    
 
-      <View style={{backgroundColor:COLORS.primary}}>
-        <Text>
-          =====================
+                                }}
+                                style={styles.X}>
+                                <View>
+                                    <Image
+                                        style={{ width: 15, height: 15 }}
+                                        source={icons.X}
+                                    />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                    numColumns={1}
+                    keyExtractor={(item, index) => index}
+                />
+            }
+
+
+            <View style={{ backgroundColor: COLORS.primary }}>
+                <Text style={{...FONTS.h1, color:COLORS.white, marginLeft:10}}>
+                    {target}
         </Text>
-      </View>
-      <FlatList
-      data = {contacts}
-      renderItem = {({item}) => (
-        <View style={styles.boxContainer}>
-          {(item.hasThumbnail) ?<Image
-          style={styles.Img}
-          source={{uri: item.thumbnailPath}}
-          /> :
-          <View style={styles.Circle}>
-            <Image
-            style={styles.Avatar}
-            source={icons.Avatar}
-            />
-          </View> }
-          <Text style={styles.Title}>
-            {item.givenName} 
-          </Text>
-          <Text> </Text>
-          <Text style={styles.Title}>
-            {item.familyName} 
-          </Text>
-          <TouchableOpacity  style={styles.Round}>
-            <View>
-            <Image
-            style={{width:15, height:15, tintColor:COLORS.black}}
-            source={icons.Done}
-            />
-          </View>
-           </TouchableOpacity>
-        </View>
-      )}
-      numColumns = {1}
-      keyExtractor = {(item, index) => index}
-      />
-    </SafeAreaView>
+            </View>
 
-  )
+            <FlatList
+                data={contacts}
+                renderItem={(item) => {
+                return(
+    
+                    <View style={styles.boxContainer}>
+
+                        {(item.item.hasThumbnail) ? <Image
+                            style={styles.Img}
+                            source={{ uri: item.item.thumbnailPath }}
+                        /> :
+                            <View style={styles.Circle}>
+                                <Image
+                                    style={styles.Avatar}
+                                    source={icons.Avatar}
+                                />
+                            </View>}
+
+                        <Text style={styles.Title}>
+                            {item.item.displayName}
+                        </Text>
+
+                       <Pressable 
+                       onPress={() => {
+                        console.log("selected==>", item.item.displayName)
+                        let temp = [...selected]
+                        console.log("temp before", temp)
+                        let data = {
+                            name :item.item.displayName ,
+                            id :item.item.recordID,
+                            img : item.item.thumbnailPath,
+                            selected:true
+                        }
+                        temp.push(data)
+                        console.log("temp after ", temp)
+                        setSelected(temp)
+                       setFlag(!flag)
+                        
+                    }}
+                    
+                    style={({ pressed }) => [
+                        {
+                          backgroundColor: pressed
+                            ? COLORS.secondary
+                            : COLORS.primary
+                        },
+                        styles.Round
+                      ]} 
+                    >
+                        <Image
+                                        style={{width:15, height:15, tintColor:COLORS.black2}}
+                                        source={icons.Done}
+                                    />
+                       </Pressable>
+
+
+                        
+
+                    </View>
+                )}}
+                numColumns={1}
+                keyExtractor={(item, index) => index}
+            />
+
+                            { selected.map(value=>{
+                                value
+                                console.log('test222', value.name)
+                            })}
+        </SafeAreaView>
+
+    )
 }
 
 export default Home;
 
-const styles=StyleSheet.create({
-  Container:{
-    flex:1,
-    backgroundColor:COLORS.black2
-  },
-  Circle:{
-    width:50, 
-    height:50, 
-    borderRadius:25, 
-    margin:5, 
-    backgroundColor:'#696969', 
-    justifyContent:'center', 
-    alignItems:'center', 
-    overflow:'hidden',
-  },
-  Img:{
-    width:50, 
-    height:50, 
-    borderRadius:25,
-    margin:5,
-  },
-  Avatar:{
-    width:52, 
-    height:52, 
-    borderRadius:50, 
-    marginTop:17,
-  },
-  Title:{
-    ...FONTS.h1, 
-    color:COLORS.white, 
-    marginTop:10,
-  },
-  boxContainer:{
-    backgroundColor:'#282828', 
-    margin:0.3, 
-    flexDirection:'row'
-  },
-  Search:{
-    height: SIZES.height*0.06,
-    width:SIZES.width*0.95, 
-    backgroundColor: COLORS.darkGrey, 
-    borderRadius:15,
-    alignSelf:'center', 
-    margin:10, 
-    color:COLORS.lightGrey,
-    fontSize:20,
-    paddingStart:30,
-  },
-  Icon:{
-    width:25, 
-    height:25, 
-    borderRadius:50, 
-    marginTop:17,
-    position:'absolute',
-    zIndex:999,
-    left:12,
-    tintColor:COLORS.lightGrey,
-  },
-  Round:{
-    backgroundColor:COLORS.secondary,
-    width:25,
-    height:25,
-    borderRadius:20,
-    justifyContent:'center', 
-    alignItems:'center',
-    right:30, 
-    position:'absolute', 
-    top:17, 
-    borderWidth:1,
-  },
-  selectContainer:{
-    flexDirection:'row', 
-    backgroundColor:COLORS.black2,
-    width:65, 
-    height:80, 
-    justifyContent:'center'
-  },
-  X:{
-    backgroundColor:COLORS.darkGrey,
-    width:20,
-    height:20,
-    borderRadius:20,
-    justifyContent:'center', 
-    alignItems:'center',
-    right:65, 
-    position:'absolute', 
-    top:2, 
-    borderWidth:1,
-    borderColor:COLORS.lightGrey
-  },
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        backgroundColor: COLORS.black2
+    },
+    Circle: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        margin: 5,
+        backgroundColor: '#696969',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
+    Img: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        margin: 5,
+    },
+    Avatar: {
+        width: 52,
+        height: 52,
+        borderRadius: 50,
+        marginTop: 17,
+    },
+    Title: {
+        ...FONTS.h1,
+        color: COLORS.white,
+        marginTop: 10,
+    },
+    boxContainer: {
+        backgroundColor: '#282828',
+        margin: 0.3,
+        flexDirection: 'row'
+    },
+    Search: {
+        height: SIZES.height * 0.06,
+        width: SIZES.width * 0.95,
+        backgroundColor: COLORS.darkGrey,
+        borderRadius: 15,
+        alignSelf: 'center',
+        margin: 10,
+        color: COLORS.lightGrey,
+        fontSize: 20,
+        paddingStart: 30,
+    },
+    Icon: {
+        width: 25,
+        height: 25,
+        borderRadius: 50,
+        marginTop: 17,
+        position: 'absolute',
+        zIndex: 999,
+        left: 12,
+        tintColor: COLORS.lightGrey,
+    },
+    Round: {
+        width: 25,
+        height: 25,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        right: 30,
+        position: 'absolute',
+        top: 17,
+        borderWidth: 1,
+    },
+    UNRound: {
+        backgroundColor: 'blue',
+        width: 25,
+        height: 25,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        right: 30,
+        position: 'absolute',
+        top: 17,
+        borderWidth: 1,
+    },
+    selectContainer: {
+        flexDirection: 'row',
+        backgroundColor: COLORS.black2,
+        width: 65,
+        height: 80,
+        marginLeft: 10,
+        justifyContent: 'center'
+    },
+    X: {
+        backgroundColor: COLORS.darkGrey,
+        width: 20,
+        height: 20,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        right: 15,
+        top: 2,
+        borderWidth: 1,
+        borderColor: COLORS.lightGrey,
+    },
 })
